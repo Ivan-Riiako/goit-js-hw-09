0c6f96062d65144a758961ac8f6e6b13b12a6848
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-11 14:47:25 +0200

    task one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-fbf
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+refs.btnStart.addEventListener('click', onClickStart);
+refs.btnStop.addEventListener('click', onClickStop);
+function onClickStart() {
+    if (timerId !== null) {
+return
+    };
+    timerId = setInterval(callback, 1000);
+
+};
+function onClickStop() {
+    clearInterval(timerId);
+    timerId = null;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function callback() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+
