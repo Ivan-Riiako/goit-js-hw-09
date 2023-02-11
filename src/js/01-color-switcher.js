f20f2582e4946f84f411d04698b0c4a01b8ef0c9
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-11 14:59:38 +0200

    11.02

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,27 @@
 const refs = {
   btnStart: document.querySelector('[data-start]'),
-    btnStop: document.querySelector('[data-stop]'),
+  btnStop: document.querySelector('[data-stop]'),
 };
+const COLOR_CHANGE_INTERVAL = 1000; 
 let timerId = null;
+
 refs.btnStart.addEventListener('click', onClickStart);
 refs.btnStop.addEventListener('click', onClickStop);
+
 function onClickStart() {
     if (timerId !== null) {
 return
     };
-    timerId = setInterval(callback, 1000);
-
+    timerId = setInterval(callback, COLOR_CHANGE_INTERVAL);
 };
 function onClickStop() {
     clearInterval(timerId);
     timerId = null;
 }
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 function callback() {
     document.body.style.backgroundColor = getRandomHexColor();
 }
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
