cf8b5fcce741f659e6df111d3ed0e666a61b4945
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-15 11:32:09 +0200

    la finalle?

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,10 +11,9 @@ function onClickSub(event) {
   const amount = Number(event.currentTarget['amount'].value);;
   const firstDelay = Number(event.currentTarget['delay'].value);
   const step = Number(event.currentTarget['step'].value);
+    let realDelay = firstDelay;
 
-  let realDelay = firstDelay;
   for (let i = 1; i <= amount; i += 1) {
-    
     createPromise(i, realDelay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -22,25 +21,16 @@ function onClickSub(event) {
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-  realDelay += step;
-    
+    realDelay+=step
   }
 }  
-    
-
-
-
-   
-
-
 
 function createPromise(position, delay) {
   return new Promise((res, rej) => {
-  const shouldResolve = Math.random() > 0.3;
-  const backend = { position, delay };
-
+    
     setTimeout(() => {
-
+      const shouldResolve = Math.random() > 0.3;
+      const backend = { position, delay };
       if (shouldResolve) {
         // Fulfill
         res(backend);
@@ -49,45 +39,5 @@ function createPromise(position, delay) {
       rej(backend);
     }, delay);
 
-  })}
-  
-
-
-
-
-  
-
-
-
-// //createPromise(amount, delay)
-//   // .then(({ position, delay }) => {
-//   //   Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   // })
-//   // .catch(({ position, delay }) => {
-//   //   Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   // });
-
-
-// const promis = new Promise((res, rej) => {
-
-// // setTimeout(()=>{
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//     res({ position: 5, delay: 2000 });
-//   } else {
-//     // Reject
-//     rej({ position: 5, delay: 2000 });
-//   }
-// // },2000)
-
-// })
-
-
-  // createPromise(324, 2342)
-  //   .then(({ position, delay }) => {
-  //     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  //   })
-  //   .catch(({ position, delay }) => {
-  //     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  //   });
+  })
+}
